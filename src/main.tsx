2707333c1ca1e199c38a937d084484ab604ab93e
Denis Ladeira <105209483+denisfloyd@users.noreply.github.com>
2022-05-31 19:11:05 -0300

    feat: :sparkles: add sentry io for test error handling

--- src/main.tsx
+++ src/main.tsx
@@ -1,4 +1,16 @@
 import ReactDOM from "react-dom/client";
+import * as Sentry from "@sentry/react";
+import { BrowserTracing } from "@sentry/tracing";
 import App from "./App";
+
+Sentry.init({
+  dsn: "https://c9d79d4b82784510890940e02cc31ba5@o1269817.ingest.sentry.io/6460131",
+  integrations: [new BrowserTracing()],
+
+  // Set tracesSampleRate to 1.0 to capture 100%
+  // of transactions for performance monitoring.
+  // We recommend adjusting this value in production
+  tracesSampleRate: 1.0,
+});
 
 ReactDOM.createRoot(document.getElementById("root")!).render(<App />);
